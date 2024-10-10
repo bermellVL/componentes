84303223acdabb828ba52193bfc1d6b2c73855e9
Victor <vibede@alumnatflorida.es>
2024-10-10 16:16:04 +0200

    ACABEM MPROJECTE

---
+++ src/App.js
@@ -0,0 +1,52 @@
+/**
+ * Exemple d'una App amb  React Native
+ *
+ * @author Manel Viel
+ * @version 1.0  [octubre 2024]
+ * @summary Aquest model el podeu utilitzar en els vostres projectes de DIN
+ */
+
+/** IMPORTEM les llibreries necessàries */
+import React from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Partit from './components/partit/Partit';
+
+/** A la funció App, dins del return crearem la notra pantalla */
+const App = () => {
+  return (
+    <View>
+      <StatusBar/>
+      <ScrollView>
+        <View>
+          <Text style={estils.titol}> Resultats LLiga 24-25 </Text>
+          <Partit>  </Partit>
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+/** En aquesta secció, crearem els estils a aplicar als nostres components */
+const estils = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  titol: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  descripcio: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+});
+
+export default App;
---
+++ src/components/partit/Partit.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { preset } from '../../../jest.config';
+
+const Partit= ()=> {
+    return(
+        <view>
+            <text>Levante cf - Valencia cf</text>
+            <text> 5 - 0 </text>
+        </view>
+    );
+};
+
+export default Partit;
