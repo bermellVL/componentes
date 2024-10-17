da76003d71d60732c7a70b97eacda5246aa3ef96
Victor <vibede@alumnatflorida.es>
2024-10-17 15:33:53 +0200

    ejercicio11

--- src/App.js
+++ src/App.js
@@ -1,51 +1,110 @@
-/**
- * Exemple d'una App amb  React Native
- *
- * @author Manel Viel
- * @version 1.0  [octubre 2024]
- * @summary Aquest model el podeu utilitzar en els vostres projectes de DIN
- */
+import { Provider as PaperProvider, Text, TextInput, Button } from 'react-native-paper';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
-/** IMPORTEM les llibreries necessàries */
-import React from 'react';
-import {
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import Partit from './components/partit/Partit';
+const estil = 'florida';
+const isAdmin = false;
 
-/** A la funció App, dins del return crearem la notra pantalla */
-const App = () => {
+const nom = (estil, textAMostrar) => {
   return (
-    <View>
-      <StatusBar/>
-      <ScrollView>
-        <View>
-          <Text style={estils.titol}> Resultats LLiga 24-25 </Text>
-          <Partit>  </Partit>
-        </View>
-      </ScrollView>
-    </View>
+    <Text style={estil} variant='displayLarge'>{textAMostrar}</Text>
   );
 }
 
-/** En aquesta secció, crearem els estils a aplicar als nostres components */
-const estils = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const dades = (inputs) => {
+  const estilPare = () => {
+    return estil === 'florida' ? styles.florida : styles.upv;
+  }
+  
+  const colorInput = () => {
+    return estil === 'florida' ? 'white' : 'orange';
+  }
+
+  return (
+    <View style={estilPare()}>
+      {inputs.map((value, index) => (
+        <TextInput
+          key={index}
+          label={value}
+          placeholder={value}
+          textColor={colorInput()}
+        />
+      ))}
+    </View>
+  )
+}
+
+const botoSiAdmin = () => {
+  if(!isAdmin)
+    return;
+  
+  return (
+    <Button
+      style={{borderRadius: 0}}
+      icon='format-list-bulleted'
+      textColor='white'
+      buttonColor='blue'
+    >
+      INFORMES
+    </Button>
+  );
+}
+
+const App = () => {
+  const inputs = ['Email', 'Nom'];
+  const moduls2Dam = [
+    { nom: 'DIN', professor: 'Manel', hores: 120 },
+    { nom: 'ADA', professor: 'Roberto', hores: 120 },
+    { nom: 'PMDM', professor: 'Paco', hores: 100 },
+    { nom: 'PSP', professor: 'Roberto', hores: 60 },
+    { nom: 'SGE', professor: 'Belén', hores: 100 },
+    { nom: 'Anglés', professor: 'Caterina', hores: 40 },
+    { nom: 'EIE', professor: 'Ana', hores: 60 },
+    ];
+
+  return (
+    <PaperProvider>
+      {nom(styles.text, "Victor y Aaron")}
+      {dades(inputs)}
+      {botoSiAdmin()}
+      <ScrollView>
+        {moduls2Dam.map((value, index) => {
+          let colorFondo = index % 2 === 0 ? '#F48FB1' : '#F8BBD0';
+          
+          return (
+            <View style={{backgroundColor: colorFondo}}>
+              <Text>{index + 1}</Text>
+              <Text>{value.nom}</Text>
+              <Text>{value.professor}</Text>
+              <Text>{value.hores} hores</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'blue',
+    fontStyle: 'italic'
   },
-  titol: {
-    fontSize: 32,
-    fontWeight: 'bold',
+  upv: {
+    backgroundColor: 'purple',
+    fontSize: 10,
+    fontWeight: '600',
+    padding: 4,
+    paddingLeft: 12,
+    textAlign: 'left',
+    color: 'grey',
   },
-  descripcio: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  florida: {
+    backgroundColor: 'red',
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
 
