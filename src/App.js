a755eb592f16ad128f2252cc731be90cc7ab454a
Victor <vibede@alumnatflorida.es>
2024-10-17 18:14:03 +0200

    comit2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Provider as PaperProvider, Text, TextInput, Button } from 'react-native-paper';
 import { ScrollView, StyleSheet, View } from 'react-native';
+import EntradaDEmail from './components/EtradaDEmails/EntradaDEmail';
 
 const estil = 'florida';
 const isAdmin = false;
@@ -11,76 +12,11 @@ const nom = (estil, textAMostrar) => {
   );
 }
 
-const dades = (inputs) => {
-  const estilPare = () => {
-    return estil === 'florida' ? styles.florida : styles.upv;
-  }
-  
-  const colorInput = () => {
-    return estil === 'florida' ? 'white' : 'orange';
-  }
-
-  return (
-    <View style={estilPare()}>
-      {inputs.map((value, index) => (
-        <TextInput
-          key={index}
-          label={value}
-          placeholder={value}
-          textColor={colorInput()}
-        />
-      ))}
-    </View>
-  )
-}
-
-const botoSiAdmin = () => {
-  if(!isAdmin)
-    return;
-  
-  return (
-    <Button
-      style={{borderRadius: 0}}
-      icon='format-list-bulleted'
-      textColor='white'
-      buttonColor='blue'
-    >
-      INFORMES
-    </Button>
-  );
-}
-
 const App = () => {
-  const inputs = ['Email', 'Nom'];
-  const moduls2Dam = [
-    { nom: 'DIN', professor: 'Manel', hores: 120 },
-    { nom: 'ADA', professor: 'Roberto', hores: 120 },
-    { nom: 'PMDM', professor: 'Paco', hores: 100 },
-    { nom: 'PSP', professor: 'Roberto', hores: 60 },
-    { nom: 'SGE', professor: 'Belén', hores: 100 },
-    { nom: 'Anglés', professor: 'Caterina', hores: 40 },
-    { nom: 'EIE', professor: 'Ana', hores: 60 },
-    ];
-
-    return (
+  return (
     <PaperProvider>
-      {nom(styles.estilDeText, 'Manel Viel')}
-      {dades(inputs)}
-      {botoSiAdmin()}
-      <ScrollView>
-        {moduls2Dam.map((value, index) => {
-          let colorFondo = index % 2 === 0 ? '#F48FB1' : '#F8BBD0';
-          
-          return (
-            <View style={{backgroundColor: colorFondo}}>
-              <Text>{index + 1}</Text>
-              <Text>{value.nom}</Text>
-              <Text>{value.professor}</Text>
-              <Text>{value.hores} hores</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
+      {nom(styles.estilDeText, 'Manel Viel')} 
+      <EntradaDEmail />
     </PaperProvider>
   );
 }
---
+++ src/components/EtradaDEmails/EntradaDEmail.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, TextInputComponent } from 'react-native';
+import { TextInput, HelperText, IconButton } from 'react-native-paper';
+
+const EntradaDEmail = () => {
+  const [email, setEmail] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
+
+  const emailRegex = /^[a-zA-Z]+\@[a-zA-Z\_\-0-9]+\.[a-z]{2,5}$/;
+
+  const isEmailValid = (email) => emailRegex.test(email);
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        keyboardType="email-address" 
+        autoCapitalize="none"
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        right={isEmailValid(email) ? (<TextInput.Icon icon="check" color="green" /> ): null} 
+        style={styles.input}
+      />
+      
+      <HelperText type="error" visible={!isEmailValid(email) && isFocused} >
+      Adresa email incorrecta
+      </HelperText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+  },
+  input: {
+    backgroundColor: 'white',
+  },
+});
+
+export default EntradaDEmail;
